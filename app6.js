4393b771826292621ec562c421945bc59163e0ab
Yolanda Barrera <136383160+YolandaBarrera@users.noreply.github.com>
2025-08-26 16:03:50 -0600

    Add files via upload
    Suma, resta, multiplicación y división en JavaScript

---
+++ app6.js
@@ -0,0 +1,11 @@
+let var1 = parseFloat (prompt("Ingresa primer valor"));
+let var2 = parseFloat (prompt("Ingresa segundo valor"));
+let sum = var1 + var2;
+let rest = var1 - var2;
+let mul =  var1 * var2;
+let div =  var1 / var2;
+
+console.log("Resultado de suma: " + sum);
+console.log("Resultado de resta: " + rest);
+console.log("Resultado de multiplicacion: " + mul);
+console.log("Resultado de division: " + div);
