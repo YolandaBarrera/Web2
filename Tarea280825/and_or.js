4a9c3d6ea1a608980dd06034a51fdd5419d2bed7
Yolanda Barrera <136383160+YolandaBarrera@users.noreply.github.com>
2025-08-30 14:30:11 -0600

    Add files via upload
    and_or contiene: El ejercicio de AND y OR
    Operaciones contiene: Ingresa 3 números, compara cual es el mayor, cual es el menor y si hay 2 o mas iguales
    El index para visualizar en navegador

---
+++ Tarea280825/and_or.js
@@ -0,0 +1,26 @@
+//AND
+var logico = 20 == 10 && 8 == 3;
+console.log(logico);
+
+var logico = 5 > 1 && 8 == 3;
+console.log(logico);
+
+var logico = 1 == 10 &&  10 > 3;
+console.log(logico);
+
+var logico = 10 < 20 &&  2 == 2;
+console.log(logico);
+
+//OR
+var logico = 15 > 17 || 5 < 2;
+console.log(logico);
+
+var logico = 8 == 8 || 7 > 1;
+console.log(logico);
+
+var logico = 8 < 10 ||  4 == 4;
+console.log(logico);
+
+var logico = 2 == 2 || 8 > 4;
+console.log(logico);
+
